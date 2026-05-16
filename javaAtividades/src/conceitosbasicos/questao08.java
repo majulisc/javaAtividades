38093c41e2016018b50945244177fb06f870a86d
Maria Júlia S Carvalho <mariajuliasouzadecarvalho1@gmail.com>
2026-05-16 11:28:40 -0300

    questao 08 nao finalizada

---
+++ javaAtividades/src/conceitosbasicos/questao08.java
@@ -0,0 +1,13 @@
+package conceitosbasicos;
+import java.util.Scanner;
+
+public class questao08 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite uma sequencia de numeros separados por espaço ex: 1 2 3 ...");
+        String [] inteiros = scan.nextLine(); // continuar
+
+
+    }
+
+}
