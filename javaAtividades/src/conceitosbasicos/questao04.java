647d2287394a881c64a25324843fc04c7de55599
Maria Júlia S Carvalho <mariajuliasouzadecarvalho1@gmail.com>
2026-05-16 10:44:28 -0300

    questao 04

--- javaAtividades/src/conceitosbasicos/questao04.java
+++ javaAtividades/src/conceitosbasicos/questao04.java
@@ -1,4 +1,24 @@
 package conceitosbasicos;
+
+import java.util.Scanner;
+
 public class questao04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite dois numeros inteiros: ");
+        int numero1 = scan.nextInt();
+        int numero2 = scan.nextInt();
+        int x = numero1 + numero2;
+
+        System.err.println("A soma dos numeros é = " + x);
+        scan.close();
+
+
+
+
+    }
+    
+
+
 
 }
