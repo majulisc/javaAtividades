5db2f16b0769ccff43e279958ac8cf61dbc5dd45
Maria Júlia S Carvalho <mariajuliasouzadecarvalho1@gmail.com>
2026-05-20 14:16:17 -0300

    questao 6

--- javaAtividades/src/estruturadedecisao/questao06.java
+++ javaAtividades/src/estruturadedecisao/questao06.java
@@ -1,5 +1,27 @@
 package estruturadedecisao;
+import java.util.Scanner;
 
 public class questao06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite seu nome:");
+        String nome1 = scan.nextLine();
+        System.out.println("Digite seu nome:");
+        String nome2 = scan.nextLine();
+        if (nome1.length() > nome2.length()){
+            System.out.println("Nome 2 é maior");
+         }
+        else if (nome2.length() < nome1.length()){
+            System.out.println("Nome 1 é maior");
+           
+        }
+        else {
+            System.out.println("O tamanho é igual");
+        }
+        scan.close();
+
+       
+
+    }
 
 }
